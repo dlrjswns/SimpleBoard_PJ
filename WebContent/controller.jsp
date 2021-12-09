e487986bf79d4c0ca2abdc79f59c4f0ae2c7e855
이건준 <leekyunjun@gmail.com>
2021-12-09 18:52:47 +0900

    0.4v Filter를 이용해 초기화매개변수사용

--- WebContent/controller.jsp
+++ WebContent/controller.jsp
@@ -1,8 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8" import="java.util.*,model.board.BoardDAO, model.board.BoardVO" %>
-<%
-	request.setCharacterEncoding("UTF-8");
-%>
+
 <jsp:useBean id="dao" class="model.board.BoardDAO" />
 <jsp:useBean id="vo" class="model.board.BoardVO" />
 <jsp:setProperty property="*" name="vo"/>
---
+++ src/filter/EncodingFilter.java
@@ -0,0 +1,49 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+@WebFilter("*.jsp")
+public class EncodingFilter implements Filter {
+	private String encoding;
+    /**
+     * Default constructor. 
+     */
+    public EncodingFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		request.setCharacterEncoding(encoding);
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		this.encoding = fConfig.getServletContext().getInitParameter("encoding");
+		//xml파일에 초기화매개변수로 넣어둔 encoding값을 가져와서 this.encoding에 넣어주는 역할
+	}
+
+}
